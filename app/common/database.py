89255a1ac91abec3bfe42f9d96d442d146b58402
Taiye <taiyeoguns@yahoo.com>
2025-02-10 13:52:17 +0000

    update model

--- app/common/database.py
+++ app/common/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
 from config import CONFIG
 
 ENGINE = create_async_engine(CONFIG.DATABASE_URL)
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 async def get_session():
--- app/common/models.py
+++ app/common/models.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from uuid import uuid4
 
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import String, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy_utils import UUIDType
 
 from app.common.database import Base
@@ -10,7 +12,12 @@ from app.common.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    uuid = Column(UUIDType(), unique=True, index=True, nullable=False, default=uuid4)
-    name = Column(String(50))
-    created_at = Column(DateTime, default=datetime.utcnow)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[UUID] = mapped_column(
+        UUIDType(), unique=True, index=True, nullable=False, default=uuid4
+    )
+    name: Mapped[str] = mapped_column(String(50))
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
